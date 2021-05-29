61f049a74440935a2f4feebe52f229762fc3966b
unknown <tufailmirr@gmail.com>
2021-05-29 20:27:17 +0530

    final changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -47,7 +47,7 @@ const About = ({ data }) => {
             </div>
             <div className="columns download">
               <p>
-                <a href={resumeDownload} className="button">
+                <a href={resumeDownload} target="_blank" rel="noopener noreferrer" className="button">
                   <i className="fa fa-download"></i>Download Resume
                 </a>
               </p>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,7 +23,7 @@ const Footer = ({ data }) => {
             <li>
               Made by{" "}
               <a title="PAPA" href="http://www.papareact.com/">
-                PAPA
+              Tufail Ahmad Mir
               </a>
             </li>
           </ul>
